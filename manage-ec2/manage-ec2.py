7cdf33dc7fcfbf1f5181621cf528e953164dd202
SALEH <sssxhn@gmail.com>
2019-04-06 02:35:40 +0300

    Wait for instances to stop and start during snapshot process

--- manage-ec2/manage-ec2.py
+++ manage-ec2/manage-ec2.py
@@ -79,15 +79,27 @@ def instances():
 	help="Create snapshots of all volumes")
 @click.option('--project', default=None,
 	help="Only instances for project (tag Project:<name>)")
-def create_snapshots(project):
+def create_snapshot(project):
 	"Create snapshots for EC2 instances"
 
 	instances = filter_instances(project)
 
 	for i in instances:
+		print("Stopping {0}...".format(i.id))
+
+		i.stop()
+		i.wait_until_stopped()
+
 		for v in i.volumes.all():
-			print("Creating snapshot of {0}".format(v.id))
-			v.create_snapshots(Description="Created by manage-ec2 3000")
+			print("  Creating snapshot of {0}".format(v.id))
+			v.create_snapshot(Description="Created by manage-ec2 3000")
+
+		print("Starting {0}...".format(i.id))
+
+		i.start()
+		i.wait_until_running()
+
+	print("Job's done!")
 
 	return
 
